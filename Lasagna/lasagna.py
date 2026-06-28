dd36c79503a72fa0a8f91a81933292bb1eaa8206
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-28 09:14:39 +0500

    added the first function in the lasagna.py

---
+++ Lasagna/lasagna.py
@@ -0,0 +1,21 @@
+
+EXPECTED_BAKE_TIME = 40
+
+
+def bake_time_remaining(elpased_bake_time):
+    """Calculate the bake time remaining.
+
+    Parameters:
+        elapsed_bake_time (int): The baking time already elapsed.
+
+    Returns:
+        int: The remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    return int(EXPECTED_BAKE_TIME - elpased_bake_time)
+
+
