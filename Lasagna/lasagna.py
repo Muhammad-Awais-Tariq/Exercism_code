0d6678622aa1bb4d89ea54c0a44c26feccbbe733
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-28 09:15:48 +0500

    added the last function in lasagna.py

--- Lasagna/lasagna.py
+++ Lasagna/lasagna.py
@@ -33,4 +33,21 @@ def preparation_time_in_minutes(number_of_layers):
     return PREPARATION_TIME * number_of_layers
 
 
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculate the elapsed cooking time.
+    
+    Parameters:
+        number_of_layers (int): The number of layers in the lasagna.
+        elapsed_bake_time (int): Time the lasagna has been baking in the oven.
+    
+    Returns:
+        int: The total time elapsed (in minutes) preparing and baking.
 
+    This function takes two integers representing the number of lasagna 
+    layers and the time already spent baking the lasagna. It calculates 
+    the total elapsed minutes spent cooking (preparing + baking).
+    
+    """
+    
+    prepration_time = preparation_time_in_minutes(number_of_layers)
+    return prepration_time + elapsed_bake_time
