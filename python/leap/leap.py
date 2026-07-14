363ea07e2892ce4027bcf8f4c94760636635edf1
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-14 10:40:52 +0500

    added the function docstring

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,10 @@
 def leap_year(year):
-    pass
+    """ Determine whether the given year is leap year or not.
+    
+    Parameters:
+        year(int) : The given year.
+    
+    Returns:
+        bool: True if the year is a leap year, otherwise False.
+    """
+    
