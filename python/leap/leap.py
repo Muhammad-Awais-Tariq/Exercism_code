5615c574bee404417290e4d4b62860d1003242c0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-14 10:45:38 +0500

    done the function

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -8,3 +8,11 @@ def leap_year(year):
         bool: True if the year is a leap year, otherwise False.
     """
     
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    
+    return False
