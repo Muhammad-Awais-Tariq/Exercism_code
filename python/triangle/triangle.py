be711a2a8945a9f66fcd3a0d708b38ea481f0bcc
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-14 11:00:32 +0500

    added the validty check

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,3 +1,28 @@
+
+def check_valid_triangle(sides):
+    """ Checks if the given sides are valid for triangle.
+
+    Parameters:
+        sides(List) : sides of the triangle.
+    
+    Returns:
+        bool: True if the sides are valid else False.
+    """
+
+    for side in sides:
+        if side <= 0:
+            return False
+    
+    if sides[0] + sides[1] < sides[2]:
+        return False
+    elif sides[1] + sides[2] < sides[0]:
+        return False
+    elif sides[0] + sides[2] < sides[1]:
+        return False
+
+    return True
+
+
 def equilateral(sides):
     pass
 
