4bdbe5909a00c6df3a770cac89a83de96cab0dd4
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-15 09:33:49 +0500

    added the first function

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,5 +1,17 @@
 def square(number):
-    pass
+    """ Returns the number of grains on a give square
+    
+    Parameters:
+        number(int) : The square of the chessboard
+    
+    Return:
+        int: The number of grains on the given square.
+    """
+
+    if 1 <= number <= 64:
+        return 2 ** (number - 1)
+    else:
+        raise ValueError("square must be between 1 and 64")    
 
 
 def total():
