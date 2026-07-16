6d3cfb5c8f07375de1bb1d1e165d63e6e293ae92
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-16 15:16:30 +0500

    added the docstring

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,13 @@
 def is_armstrong_number(number):
+    """Determine whether a given number is an Armstrong number.
+
+    Parameters:
+        number (int): The given number.
+
+    Returns:
+        bool: True if the number is an Armstrong number; otherwise, False.
+
+    An Armstrong number is a number that is the sum of its own digits,
+    each raised to the power of the number of digits.
+    """
     pass
