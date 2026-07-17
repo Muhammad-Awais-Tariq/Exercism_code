c90ab90deac6d194e3396a18fb7c26d92a0dc455
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-17 10:49:59 +0500

    done the final version

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -11,17 +11,15 @@ def steps(number):
         If the number is even, divide it by 2.
         If the number is odd, multiply it by 3 and add 1.
     """
-    if number >= 1:
-        steps = 0
-
-        while number != 1:
-            if number % 2 == 0:
-                number = number / 2
-            else:
-                number = (number * 3) + 1
-            steps += 1
-        
-        return steps
-    
-    else:
+    if number < 1:
         raise ValueError("Only positive integers are allowed")
+    
+    steps = 0
+    while number != 1:
+        if number % 2 == 0:
+            number //= 2
+        else:
+            number = (number * 3) + 1
+        steps += 1
+        
+    return steps
