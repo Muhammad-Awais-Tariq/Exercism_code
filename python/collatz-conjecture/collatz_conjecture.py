68bc4f530571471641d9d01263d61f2f337edb93
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-17 09:52:02 +0500

    added the function docstring

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    """ Return the number of steps required to reach 1 using the Collatz conjecture.
+
+    Parameters:
+        number(int): The give number.
+    
+    Returns:
+        int: The number of steps required to reach 1.
+    
+    Collatz conjecture rules:
+        If the number is even, divide it by 2.
+        If the number is odd, multiply it by 3 and add 1.
+    """
