5779c1ab142e4cea0cac7ed132d3ef65f3af6811
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-18 10:26:18 +0500

    added the doc string

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,16 @@
 def response(hey_bob):
-    pass
+    """Determine how Bob responds to a given statement.
+
+    Parameters:
+        hey_bob (str): The statement addressed to Bob.
+
+    Returns:
+        str: Bob's response.
+
+    Response rules:
+        - "Sure." if the statement is a question.
+        - "Whoa, chill out!" if the statement is yelled.
+        - "Calm down, I know what I'm doing!" if the statement is a yelled question.
+        - "Fine. Be that way!" if the statement is empty or contains only whitespace.
+        - "Whatever." for anything else.
+    """
