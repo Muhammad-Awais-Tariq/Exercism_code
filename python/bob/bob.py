db443751623926bba308279966d0e585f030fdc3
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-18 10:32:28 +0500

    done the third response

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -20,3 +20,6 @@ def response(hey_bob):
 
     if hey_bob.isupper():
         return "Whoa, chill out!"
+    
+    if hey_bob.isupper() and hey_bob.strip().endswith("?"):
+        return "Calm down, I know what I'm doing!"
