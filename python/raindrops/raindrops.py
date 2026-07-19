0d06598a2cd2279f15918e5266e77d08d397e1bd
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-19 15:26:34 +0500

    done the final version

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -20,10 +20,14 @@ def convert(number):
 
     if number % 3 == 0:
         result += "Pling"
-    elif number % 5 == 0:
-        result += "Plang"    
-    elif number % 7 == 0:
-        result += "Plong" 
-    else:
-        result == str(number)      
+
+    if number % 5 == 0:
+        result += "Plang"
+
+    if number % 7 == 0:
+        result += "Plong"
+
+    if not result:
+        result = str(number)
+
     return result
