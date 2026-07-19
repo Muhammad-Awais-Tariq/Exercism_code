a5365d2fb14810f7ea624feccf8d6ba5c87989fb
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-19 15:15:09 +0500

    added the doc string

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,17 @@
 def convert(number):
-    pass
+    """Convert a number into its corresponding raindrop sounds.
+
+    Parameters:
+        number (int): The number to convert.
+
+    Returns:
+        str: The corresponding raindrop sounds, or the number as a string
+        if it is not divisible by 3, 5, or 7.
+
+    Rules:
+        - If the number is divisible by 3, add "Pling" to the result.
+        - If the number is divisible by 5, add "Plang" to the result.
+        - If the number is divisible by 7, add "Plong" to the result.
+        - If the number is not divisible by 3, 5, or 7, return the number
+          as a string.
+    """
