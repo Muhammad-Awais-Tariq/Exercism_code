8de0e6d868b94b6c15c41b15369ecf904d2baca9
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-24 08:52:04 +0500

    added the doc string

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,10 @@
 def rotate(text, key):
-    pass
+    """Return the text encrypted using a Caesar cipher.
+
+    Parameters:
+        text (str): The text to encrypt.
+        key (int): The rotation key used for encryption.
+
+    Returns:
+        str: The encrypted text.
+    """
