fbe2c61c47dac115dbd46615af7aedc751e3fe1f
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-24 08:16:03 +0500

    added the doc string

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,9 @@
 def is_valid(isbn):
-    pass
+    """Check if the given number is an valid isbn number.
+
+    Parameters:
+        isbn (str): The number to check.
+
+    Returns:
+        bool: True if the number is an isbn, otherwise False.
+    """
