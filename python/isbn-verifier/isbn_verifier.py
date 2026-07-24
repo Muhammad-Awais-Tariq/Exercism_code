49c71daa4fbff6e4a64ee8c9f6079725f0a154ab
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-24 08:36:06 +0500

    added the x check

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -7,14 +7,21 @@ def is_valid(isbn):
     Returns:
         bool: True if the number is an isbn, otherwise False.
     """
-    formatted_number = isbn.replace("-" , "")[::-1]
+    formatted_number = isbn.replace("-" , "")[::-1].lower()
 
     if len(formatted_number) != 10:
         return False
-    
+
+    if "x" in formatted_number:
+        if formatted_number.find("x") != 0:
+            return False
+
+        if formatted_number.count("x") != 1:
+            return False
+
     sum_number = 0
 
     for multiplier in range(1,11):
-        sum_number += multiplier * int(formatted_number[multiplier-1].replace("X" , "10"))
+        sum_number += multiplier * int(formatted_number[multiplier-1].replace("x" , "10"))
     
     return sum_number % 11 == 0
