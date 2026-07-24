5f2798cd6999833bf5523b2f748bb25046d32f51
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-24 08:32:00 +0500

    added the length check

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -7,13 +7,14 @@ def is_valid(isbn):
     Returns:
         bool: True if the number is an isbn, otherwise False.
     """
+    formatted_number = isbn.replace("-" , "")[::-1]
 
-    formatted_number = isbn.replace("-" , "")[::-1].replace("X" , "10")
+    if len(formatted_number) != 10:
+        return False
+    
     sum_number = 0
 
     for multiplier in range(1,11):
-        sum_number += multiplier * int(formatted_number[multiplier-1])
-
+        sum_number += multiplier * int(formatted_number[multiplier-1].replace("X" , "10"))
+    
     return sum_number % 11 == 0
-
-print(is_valid("3-598-21507-X"))
