8575d2d5e59e36cc373274cfe577c615d54c3aff
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-25 15:17:36 +0500

    added the doc string

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,4 +1,12 @@
 def color_code(color):
+    """Return the code corresponding to the given color.
+
+    Parameters:
+        color (str): The color whose code to return.
+
+    Returns:
+        int: The color code.
+    """
     pass
 
 
