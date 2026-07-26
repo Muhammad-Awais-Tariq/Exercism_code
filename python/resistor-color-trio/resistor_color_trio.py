3db614d28d17ee930a0042b6f5032f3f9dfdcfd6
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-26 09:52:57 +0500

    add the helper function

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -1,3 +1,24 @@
+
+COLORS = ["black" , "brown" , "red" , "orange" , "yellow" , "green" , "blue" , "violet" , "grey" , "white"]
+
+def value(colors):
+    """Return the resistor value represented by the first two colors.
+
+    Parameters:
+        colors (list): The resistor color bands.
+
+    Returns:
+        int: The resistor value represented by the first two colors.
+    """
+
+    color_value = ""
+
+    for color_index in range(2):
+        color_value += str(COLORS.index(colors[color_index]))
+
+    return int(color_value)
+
+
 def label(colors):
     """Return the resistor's resistance as a formatted label.
 
