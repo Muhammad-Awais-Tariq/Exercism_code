fa732de3034e39bab81ec501336e9c4818a6a4f8
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-27 09:01:33 +0500

    added the helper function

--- python/resistor-color-expert/resistor_color_expert.py
+++ python/resistor-color-expert/resistor_color_expert.py
@@ -1,2 +1,51 @@
+
+COLORS = ["black" , "brown" , "red" , "orange" , "yellow" , "green" , "blue" , "violet" , "grey" , "white"]
+
+def value(colors):
+    """Return the resistor value represented by the first two colors.
+
+    Parameters:
+        colors (list): The resistor color bands.
+
+    Returns:
+        int: The resistor value represented by the first two colors.
+    """
+
+    color_value = ""
+
+    for color_index in range(2):
+        color_value += str(COLORS.index(colors[color_index]))
+
+    return int(color_value)
+
+
+def label(colors):
+    """Return the resistor's resistance as a formatted label.
+
+    Parameters:
+        colors (list): The resistor color bands.
+
+    Returns:
+        str: The resistance label, including the appropriate unit.
+    """
+
+    color_value = value(colors[:2])
+    full_color_value = (color_value) * (10 **COLORS.index(colors[2]))
+
+    if not full_color_value:
+        return "0 ohms"
+
+    if full_color_value % 1000000000 == 0:
+        return f"{(full_color_value // 1000000000)} gigaohms"   
+
+    if full_color_value % 1000000 == 0:
+        return f"{(full_color_value // 1000000)} megaohms"
+        
+    if full_color_value % 1000 == 0:
+        return f"{(full_color_value // 1000)} kiloohms"
+
+    return f"{full_color_value} ohms"
+
+
 def resistor_label(colors):
     pass
