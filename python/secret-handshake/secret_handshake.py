a48add2a15bf15f478d620b8851d572f4b9a8a16
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-28 09:40:49 +0500

    added the one index and final code

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -7,3 +7,12 @@ def commands(binary_str):
     Returns:
         list: The decoded actions in the order they should be performed.
     """
+
+    code_dict = {
+        0 : "wink",
+        1 : "double blink",
+        2 : "close your eyes",
+        3 : "jump"
+    }
+
+    one_index = [i for i,char in enumerate(binary_str) if char == "1"]
