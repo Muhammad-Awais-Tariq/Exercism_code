254a04eb3483163a9349bf82f5671fb100888679
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-29 10:14:36 +0500

    done the exercise

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -8,3 +8,15 @@ def find_anagrams(word, candidates):
     Returns:
         list: The candidate words that are anagrams of the target word.
     """
+
+    anagrams = []
+    lower_word = word.lower()
+    target = sorted(lower_word)
+
+    for candidate in candidates:
+        lower_candidate = candidate.lower()
+
+        if lower_candidate != lower_word and target == sorted(lower_candidate):
+            anagrams.append(candidate)
+
+    return anagrams
