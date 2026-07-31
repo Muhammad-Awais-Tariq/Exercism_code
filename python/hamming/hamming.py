7824fc1ffa4e1f1d6433ffe33ced165db27365a4
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-31 11:17:39 +0500

    added the second approach

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -17,10 +17,5 @@ def distance(strand_a, strand_b):
 
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands must be of equal length.")
-    
-    hamming_distance = 0
-    for index in range(len(strand_a)):
-        if strand_a[index] != strand_b[index]:
-            hamming_distance += 1
 
-    return hamming_distance
+    return sum(a != b for a ,b in zip(strand_a , strand_b))
