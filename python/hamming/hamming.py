d4e26c44c4c9244d66bc94aced0dff1aff3b7a8c
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-31 11:08:11 +0500

    added the doc string

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,13 @@
 def distance(strand_a, strand_b):
-    pass
+    """Return the Hamming distance between two DNA strands.
+
+    Parameters:
+        strand_a (str): The first DNA strand.
+        strand_b (str): The second DNA strand.
+
+    Returns:
+        int: The Hamming distance between the two DNA strands.
+
+    The Hamming distance is the number of positions at which the
+    corresponding nucleotides are different.
+    """
