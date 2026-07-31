3c30e776f0d76bea5829fbf06d15c5b8536dbeb6
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-31 11:12:11 +0500

    done the basic way

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -11,3 +11,13 @@ def distance(strand_a, strand_b):
     The Hamming distance is the number of positions at which the
     corresponding nucleotides are different.
     """
+
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    
+    hamming_distance = 0
+    for letter in range(len(strand_a)):
+        if strand_a[letter] != strand_b[letter]:
+            hamming_distance +=1
+
+    return hamming_distance
