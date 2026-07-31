705bc360119744554911d75e6b3317c921ad9f43
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-31 11:19:51 +0500

    fixed the generic naming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -18,4 +18,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands must be of equal length.")
 
-    return sum(a != b for a ,b in zip(strand_a , strand_b))
+    return sum(nucleotide_a != nucleotide_b for nucleotide_a ,nucleotide_b in zip(strand_a , strand_b))
