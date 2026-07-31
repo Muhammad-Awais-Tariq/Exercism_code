176b8eedab5db1654293304c78a918b7e1b3d519
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-31 11:13:51 +0500

    fixed the code

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -8,6 +8,9 @@ def distance(strand_a, strand_b):
     Returns:
         int: The Hamming distance between the two DNA strands.
 
+    Raises:
+        ValueError: If the DNA strands are not the same length.
+
     The Hamming distance is the number of positions at which the
     corresponding nucleotides are different.
     """
@@ -16,8 +19,8 @@ def distance(strand_a, strand_b):
         raise ValueError("Strands must be of equal length.")
     
     hamming_distance = 0
-    for letter in range(len(strand_a)):
-        if strand_a[letter] != strand_b[letter]:
-            hamming_distance +=1
+    for index in range(len(strand_a)):
+        if strand_a[index] != strand_b[index]:
+            hamming_distance += 1
 
     return hamming_distance
