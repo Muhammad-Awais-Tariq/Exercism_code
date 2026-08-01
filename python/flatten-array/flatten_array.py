2b9f42c1be08cb9677efa92d384c9a4cc48576c9
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-01 08:53:23 +0500

    fixed the docstring

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,9 @@
 def flatten(iterable):
-    pass
+    """Return a flattened list with all nested lists expanded.
+
+    Parameters:
+        iterable (list): The list to flatten.
+
+    Returns:
+        list: A flattened list.
+    """
