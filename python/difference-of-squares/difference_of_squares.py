ed3a8e3390e307980ca62668df26ff003d24274c
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-02 09:27:29 +0500

    added the first function docstring

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,5 +1,13 @@
 def square_of_sum(number):
-    pass
+    """Return the square of the sum of all natural numbers up to the given number.
+
+    Parameters:
+        number (int): The upper limit of the natural numbers to sum.
+
+    Returns:
+        int: The square of the sum.
+    """
+
 
 def sum_of_squares(number):
     pass
