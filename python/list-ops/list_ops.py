c9964e5313e6081b8213418be35ab74d1a4d57a4
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-03 09:36:56 +0500

    added the first docstring

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,5 +1,13 @@
 def append(list1, list2):
-    pass
+    """Append the elements of the second list to the end of the first list.
+
+    Parameters:
+        list1 (list): The first list.
+        list2 (list): The list whose elements will be appended.
+
+    Returns:
+        list: The first list with the elements of the second list appended.
+    """
 
 
 def concat(lists):
