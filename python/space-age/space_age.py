b40f7646260d5c25d37247732d826a219a5c34b5
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-05 09:30:47 +0500

    added the class docstring

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,3 +1,9 @@
 class SpaceAge:
+    """Represent a person's age in seconds.
+
+    Store an age in seconds and provide methods to calculate
+    the equivalent age on each planet.
+    """
+    
     def __init__(self, seconds):
         pass
