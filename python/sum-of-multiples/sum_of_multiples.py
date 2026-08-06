316f3f0b8d41990dfae3da44abeb642d1b66ec94
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-06 10:56:19 +0500

    intial solution

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -9,3 +9,12 @@ def sum_of_multiples(limit, multiples):
         int: The sum of all unique multiples below the limit.
 
     """
+    total_multiples = set()
+
+    for multiple in multiples:
+        if multiple !=0:
+            for devisor in range(limit):
+                if devisor % multiple == 0 :
+                    total_multiples.add(devisor)
+
+    return sum(total_multiples)
