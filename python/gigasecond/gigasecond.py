cee9a758d1486348f660e335be377144c559f4c9
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-07 18:10:20 +0500

    done the docstring

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,10 @@
 def add(moment):
-    pass
+    """Return the date and time one gigasecond after the given moment.
+
+    Parameters:
+        moment (datetime): The starting date and time.
+
+    Returns:
+        datetime: The date and time one gigasecond later.
+
+    """
