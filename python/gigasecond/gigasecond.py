6ec79f95ef178216d63907b57e50636c000dfb39
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-07 18:15:20 +0500

    improved the code with guidelines

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,5 +1,5 @@
-
 from datetime import timedelta
+
 
 def add(moment):
     """Return the date and time one gigasecond after the given moment.
@@ -9,6 +9,6 @@ def add(moment):
 
     Returns:
         datetime: The date and time one gigasecond later.
-    """
 
-    return moment + timedelta(seconds=1e9)
+    """
+    return moment + timedelta(seconds=1_000_000_000)
