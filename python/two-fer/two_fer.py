2b41fbc352fb730f4ce3944a0b67df9684b92112
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-08 07:19:45 +0500

    added the docstring

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,9 @@
-def two_fer(name):
-    pass
+def two_fer(name="you"):
+    """Return a dialogue using the given name.
+
+    Parameters:
+        name (str): The name of the person.
+
+    Returns:
+        str: The customized dialogue.
+    """
