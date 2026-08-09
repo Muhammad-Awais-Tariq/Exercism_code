854894e5cea7907af1f1a782151231a9772c2950
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-09 12:13:10 +0500

    added the docstring

--- python/square-root/square_root.py
+++ python/square-root/square_root.py
@@ -1,2 +1,13 @@
 def square_root(number):
-    pass
+    """Find the square root of the number using the linear method.
+
+    Parameters:
+        number (int): The number that we want the square root of.
+
+    Returns:
+        int: The square root of the number.
+
+    Raises:
+        Exception: If no square root is found for the whole number.
+
+    """
