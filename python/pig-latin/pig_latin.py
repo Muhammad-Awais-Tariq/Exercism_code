e9fd28fddd062fa37cab2c44b75164c21de8b56e
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-10 16:19:48 +0500

    done the rule 2

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -14,6 +14,17 @@ def translate(text):
     if text[0] in vowels or text[0:2] in ay_words:
         return f"{text}ay"
 
+    if text[0] not in vowels and "qu" in text:
+        if text[0:2] == "qu":
+            return f"{text[2:]}{text[:2]}ay"
+        else:
+            count = 0
+            for word in range(len(text)):
+                if text[word] != "q" and text[word+1] != "u":
+                    count += 1
+                else:
+                    return f"{text[count+2:]}{text[:count+2]}ay"
+                
     count = 0
     for word in text:
         if word not in vowels:
