a24578a74ab8994c09207b741eb08770112d9a31
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-13 15:44:46 +0500

    done for the lowercase

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -8,6 +8,13 @@ def encode(plain_text):
         str: The encoded text.
     """
 
+    output_str = ""
+
+    for char in plain_text:
+        ascii_char = ord(char)
+        position = (ascii_char - 97) + 1
+        new_position = (27 - position) + 97
+        output_str += chr(new_position)
 
 def decode(ciphered_text):
     pass
