4573f50cbcc8ec310f006ccabfd963c22d732cce
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-13 15:55:03 +0500

    done the first function

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -11,10 +11,19 @@ def encode(plain_text):
     output_str = ""
 
     for char in plain_text:
-        ascii_char = ord(char)
-        position = (ascii_char - 97) + 1
-        new_position = (27 - position) + 97
-        output_str += chr(new_position)
+        if char.isalpha():
+            lowercase_char = char.lower()
+            ascii_char = ord(lowercase_char)
+            position = (ascii_char - 97) + 1
+            new_position = (27 - position) + 97
+            if char.isupper():
+                output_str += chr(new_position).upper()
+            else:
+                output_str += chr(new_position)    
+        else:        
+            output_str += char
+
+    return output_str
 
 def decode(ciphered_text):
     pass
