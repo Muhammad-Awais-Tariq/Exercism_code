97da8bb1980b8e4b9d544c5b02aacb93d7caa441
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-13 15:37:49 +0500

    added the first docstring

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,5 +1,12 @@
 def encode(plain_text):
-    pass
+    """Encode plain text using the Atbash cipher.
+
+    Parameters:
+        plain_text (str): The plain text to encode.
+
+    Returns:
+        str: The encoded text.
+    """
 
 
 def decode(ciphered_text):
