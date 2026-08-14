8080f1c6638a411bf486a48346dff91aeecf9ca7
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-14 11:40:00 +0500

    done the code for the dimond pattern

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -7,3 +7,27 @@ def rows(letter):
     Returns:
         str: The diamond pattern.
     """
+
+    for i in range(5):
+        for j in range(5-i):
+            print("*" , end=" ")
+        for l in range(i):
+            print("-" , end=" ")
+        for m in range(i):
+            print("-" , end=" ")
+        for n in range(5-i):
+            print("*" , end=" ")
+        print()    
+
+    for i in range(6):
+        for l in range(i):
+            print("*" , end=" ")
+        for j in range(5-i):
+            print("-" , end=" ")
+        for m in range(5-i):
+            print("-" , end=" ")
+        for l in range(i):
+            print("*" , end=" ")            
+        print()
+        
+rows("a")
