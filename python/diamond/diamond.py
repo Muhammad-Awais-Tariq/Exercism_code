2688c19ad969d1fc16c26888ec327a834f2838a7
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-14 12:37:33 +0500

    done the upper row building

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -9,16 +9,18 @@ def rows(letter):
     """
 
     letter_position = ord(letter) - 65
+    result = []
 
     for rows in range(letter_position+1):
+        row = ""
         for first_column in range(letter_position-rows):
-            print(" " , end=" ")
-        print(chr (rows + 65) , end=" ")
+            row += " "
+        row += chr (rows + 65)
         for second_column in range(2 * rows - 1):
-            print(" " , end=" ")
+            row += " "
         if rows != 0:
-            print(chr (rows + 65) , end=" ")
-        print()
+            row += chr (rows + 65)
+        result.append(row)
 
     for rows in range(letter_position - 1 , -1 , -1):
         for first_column in range(letter_position-rows):
