5eaa199e067e302dd42e10e11f8f5daf488b5f5a
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-14 12:38:20 +0500

    done the lower part

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -23,13 +23,14 @@ def rows(letter):
         result.append(row)
 
     for rows in range(letter_position - 1 , -1 , -1):
+        row = ""
         for first_column in range(letter_position-rows):
-            print(" " , end=" ")
-        print(chr (rows + 65) , end=" ")
+            row += " "
+        row += chr (rows + 65)
         for second_column in range(2 * rows - 1):
-            print(" " , end=" ")
+            row += " "
         if rows != 0:
-            print(chr (rows + 65) , end=" ")
-        print()
+            row += chr (rows + 65)
+        result.append(row)
 
 rows("E")
