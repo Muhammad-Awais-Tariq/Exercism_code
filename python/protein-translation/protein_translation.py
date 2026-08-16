b7405dd6de394155ffff9052b63c98204aea1c00
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-16 11:24:14 +0500

    Added the condon translation

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -7,3 +7,23 @@ def proteins(strand):
     Returns:
         list: A list of amino acids.
     """
+    
+    codon_translation = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP",
+    }
