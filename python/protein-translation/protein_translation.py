28224bc515dcfaae6ddb1bfb2808c5baa130e0de
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-16 11:21:18 +0500

    added the dosctring

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,9 @@
 def proteins(strand):
-    pass
+    """Convert an RNA strand into a list of proteins.
+
+    Parameters:
+        strand (str): The RNA sequence.
+
+    Returns:
+        list: A list of amino acids.
+    """
