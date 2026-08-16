2ce581977e2183cb2f57dd8fa78703a5b6530879
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-16 11:30:05 +0500

    done the exercise

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -7,7 +7,7 @@ def proteins(strand):
     Returns:
         list: A list of amino acids.
     """
-    
+
     codon_translation = {
         "AUG": "Methionine",
         "UUU": "Phenylalanine",
@@ -27,3 +27,16 @@ def proteins(strand):
         "UAG": "STOP",
         "UGA": "STOP",
     }
+
+    Amino_acids = []
+
+    for nucleotide in range(0 , len(strand) , 3):
+        condon = strand[nucleotide : nucleotide + 3]
+
+        if condon in codon_translation.keys():
+            if codon_translation[condon] == "STOP":
+                return Amino_acids
+
+            Amino_acids.append(codon_translation[condon])
+
+    return Amino_acids
