3d8d5ca2b6fe292d5b4a07d2a1d9dcdee40204f7
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-17 16:12:47 +0500

    done the factors

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -7,3 +7,21 @@ def factors(value):
     Returns:
         list: The prime factors of the number.
     """
+
+    all_factors = []
+    required_number = value
+
+    for number in range(2 , value):
+
+        while required_number % number == 0:
+            all_factors.append(number)
+            required_number //= number
+
+        if required_number == 1:
+            return all_factors
+
+    if required_number > 1:
+        all_factors.append(required_number)
+        
+    return all_factors
+
