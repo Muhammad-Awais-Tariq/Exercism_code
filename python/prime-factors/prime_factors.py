7dad31f5c4c27c4661dafe172677d091f8d05efe
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-17 16:03:12 +0500

    added the docstring

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,9 @@
 def factors(value):
-    pass
+    """Find the prime factors of a given number.
+
+    Parameters:
+        value (int): The number to find the prime factors of.
+
+    Returns:
+        list: The prime factors of the number.
+    """
