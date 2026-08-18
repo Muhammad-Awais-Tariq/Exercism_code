6ff9a1888bd6b35a9e6c98c65d4cc4e12a3201ae
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-18 19:51:16 +0500

    done the exercise

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,3 +1,6 @@
+from string import punctuation
+
+
 def abbreviate(words):
     """Return the acronym formed from the first letter of each word.
 
@@ -7,3 +10,7 @@ def abbreviate(words):
     Returns:
         str: The acronym.
     """
+    words = words.replace("-", " ")
+    normalized_words = words.translate(words.maketrans("", "", punctuation))
+
+    return "".join(word[0].upper() for word in normalized_words.split())
