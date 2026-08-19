f66fc8fd87947206e6acae7a1fc7895998fe0d18
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-19 13:38:20 +0500

    added the docstring

---
+++ python/series/series.py
@@ -0,0 +1,10 @@
+def slices(series, length):
+    """Return all consecutive slices of the given length from the series.
+
+    Parameters:
+        series (str): The series to create slices from.
+        length (int): The length of each slice.
+
+    Returns:
+        list: A list containing the consecutive slices.
+    """
