6801fc4c80cd626c5b2894b90dfcacd511d4c6c5
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-20 15:49:13 +0500

    done the encoding

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -11,3 +11,18 @@ def encode(string):
     Returns:
         str: The run-length encoded string.
     """
+
+    result = ""
+    i = 0
+    while i < len(string):
+        count = 1
+        while i + 1 < len(string) and string[i] == string[i+1]:
+            count += 1
+            i += 1
+        if count != 1:
+            result += f"{count}{string[i]}"
+        else:
+            result += f"{string[i]}"
+        i+= 1
+
+    return result
