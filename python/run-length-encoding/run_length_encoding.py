7b8930108c84aca0451cee37e7ee90a57da1113c
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-20 16:00:01 +0500

    done the decoding

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -8,6 +8,27 @@ def decode(string):
         str: The decoded string.
     """
 
+    result = ""
+    i = 0
+
+    while i < len(string):
+        count = ""
+
+        while i < len(string) and string[i].isdigit():
+            count += string[i]
+            i += 1
+
+        if count:
+            count = int(count)
+        else:
+            count = 1
+
+        result += string[i] * count
+        i += 1
+
+    return result
+
+
 def encode(string):
     """Encode a string using run-length encoding.
 
