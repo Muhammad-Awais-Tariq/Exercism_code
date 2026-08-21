9a9609bdb1b701076ce4aa85de64e3a5a06d3ba5
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-21 20:02:58 +0500

    added the doc string

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,2 +1,12 @@
 def prime(number):
-    pass
+    """Return the nth prime number.
+
+    Parameters:
+        number (int): The position of the prime number to find (1-indexed).
+
+    Returns:
+        int: The nth prime number.
+
+    Raises:
+        ValueError: If number is less than 1.
+    """
