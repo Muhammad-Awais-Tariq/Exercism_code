b45f5b8337c4749f83bc5e116e7feef9bc688515
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-22 21:29:03 +0500

    added the helpefull arrays

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -8,3 +8,13 @@ def recite(start_verse, end_verse):
     Returns:
         list[str]: The requested verses, in order.
     """
+
+    ordinals = ["first", "second", "third", "fourth", "fifth", "sixth",
+            "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+
+    gifts = ["a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens",
+            "four Calling Birds", "five Gold Rings", "six Geese-a-Laying",
+            "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing",
+            "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"]
+
+    
