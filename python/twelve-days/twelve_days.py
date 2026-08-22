3c473457e37114918608f31c0087293e971efe13
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-22 21:21:00 +0500

    added the doc string

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,2 +1,10 @@
 def recite(start_verse, end_verse):
-    pass
+    """Return the requested verses of the The Twelve Days of Christmas.
+
+    Parameters:
+        start_verse (int): The first verse to recite.
+        end_verse (int): The last verse to recite.
+
+    Returns:
+        list[str]: The requested verses, in order.
+    """
