593c888a66d0d85a5ffd438d79774aa1dafb5620
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-22 21:36:25 +0500

    done

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -17,4 +17,16 @@ def recite(start_verse, end_verse):
             "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing",
             "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"]
 
-    
+    verses = []
+    for verse_num in range(start_verse, end_verse + 1):
+        index = verse_num - 1
+        verse = f"On the {ordinals[index]} day of Christmas my true love gave to me: "
+
+        gift_list = [gifts[i] for i in range(index, -1, -1)]
+        if len(gift_list) > 1:
+            gift_list[-1] = "and " + gift_list[-1]
+
+        verse += ", ".join(gift_list) + "."
+        verses.append(verse)
+
+    return verses
