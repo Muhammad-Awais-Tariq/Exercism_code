ef5a526e1aa7c79202b5518f4afc1d9cc2ce258b
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-24 09:36:57 +0500

    added the keys

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -7,3 +7,22 @@ def roman(number):
     Returns:
         str: The Roman numeral representation.
     """
+
+    roman_map = {
+        1 : "I",
+        5 : "V",
+        10 : "X",
+        50 : "L",
+        100 : "C",
+        500 : "D",
+        1000 : "M"
+    }
+
+    special_map = {
+        4 : "IV",
+        9 : "IX",
+        40 : "XL",
+        90 : "XC",
+        400 : "CD",
+        900 : "CM"        
+    }
