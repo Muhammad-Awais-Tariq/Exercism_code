80f7287242d8a129d4b6df1e554209950068187e
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-24 09:30:55 +0500

    added the docstring

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,3 +1,9 @@
 def roman(number):
-    pass
+    """Convert an Arabic numeral to a Roman numeral.
 
+    Parameters:
+        number (int): The Arabic numeral to convert.
+
+    Returns:
+        str: The Roman numeral representation.
+    """
