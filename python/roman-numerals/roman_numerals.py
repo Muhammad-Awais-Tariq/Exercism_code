39ed471410ccad57c157d89a9d40cbb6bede4866
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-24 09:49:57 +0500

    fixed the mapping and special case

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -9,27 +9,20 @@ def roman(number):
     """
 
     roman_map = {
-        1 : "I",
-        5 : "V",
-        10 : "X",
-        50 : "L",
-        100 : "C",
-        500 : "D",
-        1000 : "M"
+        1: "I",
+        4: "IV",
+        5: "V",
+        9: "IX",
+        10: "X",
+        40: "XL",
+        50: "L",
+        90: "XC",
+        100: "C",
+        400: "CD",
+        500: "D",
+        900: "CM",
+        1000: "M"
     }
-
-    special_map = {
-        4 : "IV",
-        9 : "IX",
-        40 : "XL",
-        90 : "XC",
-        400 : "CD",
-        900 : "CM"        
-    }
-
-    if number in special_map:
-        return special_map[number]
 
     if number in roman_map:
         return roman_map[number]
-    
