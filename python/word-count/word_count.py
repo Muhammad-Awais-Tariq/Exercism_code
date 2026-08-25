86f5ece60b015ba418ef774109a8d4f9bba6c754
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-25 18:32:30 +0500

    added the docstring

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,9 @@
 def count_words(sentence):
-    pass
+    """Takes a sentence and return the word count of all the words.
+
+    Parameters:
+        sentence (str): The given sentence.
+
+    Returns:
+        dict: The frequency of words in the sentence.
+    """
