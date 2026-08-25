55db8ea53580ebe8ec4888bd91fcdca24b589821
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-25 18:42:21 +0500

    done the exercise

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,3 +1,6 @@
+
+import re
+
 def count_words(sentence):
     """Takes a sentence and return the word count of all the words.
 
@@ -7,3 +10,14 @@ def count_words(sentence):
     Returns:
         dict: The frequency of words in the sentence.
     """
+
+    hash_map = {}
+    words = re.findall(r"[a-z0-9]+(?:'[a-z0-9]+)?", sentence.lower())
+
+    for word in words:
+        if word in hash_map:
+            hash_map[word] += 1
+        else:
+            hash_map[word] = 1
+
+    return hash_map
