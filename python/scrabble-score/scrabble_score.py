b6f5742cb8d28deedac03c0042b3a939dbc5b7f8
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-26 17:07:09 +0500

    added the more optimized version of scrabble score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -8,20 +8,14 @@ def score(word: str) -> int:
         int: The scrabble score.
     """
 
-    letter_values = {
-        ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
-        ("D", "G"): 2,
-        ("B", "C", "M", "P"): 3,
-        ("F", "H", "V", "W", "Y"): 4,
-        ("K",): 5,
-        ("J", "X"): 8,
-        ("Q", "Z"): 10,
+    LETTER_VALUES = {
+        "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
+        "D": 2, "G": 2,
+        "B": 3, "C": 3, "M": 3, "P": 3,
+        "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+        "K": 5,
+        "J": 8, "X": 8,
+        "Q": 10, "Z": 10,
     }
 
-    flatten = {k : v for key , v in letter_values.items() for k in key}
-
-    final_score = 0
-    for char in word:
-        final_score += flatten[char.upper()]
-
-    return final_score
+    return sum(LETTER_VALUES.get(char) for char in word.upper())
