dda92d6688d75b79ca158418809a5c903f24aafb
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-26 17:00:04 +0500

    added the doc string

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,9 @@
-def score(word):
-    pass
+def score(word: str) -> int:
+    """Takes a word and return the Scrabble score.
+
+    Parameters:
+        word (str): The word we want score of.
+    
+    Returns:
+        int: The scrabble score.
+    """
