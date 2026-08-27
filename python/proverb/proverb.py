c7d75dd7954b8fe7065f3a88fe67258f0ff81f52
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-27 12:38:12 +0500

    added the docstring

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,2 +1,10 @@
-def proverb():
-    pass
+def proverb(*items, qualifier=None):
+    """Generate a proverb from the given words.
+
+    Parameters:
+        items (str): The words used to construct the proverb.
+        qualifier (str, optional): An optional word added to the final line.
+
+    Returns:
+        str: The generated proverb.
+    """
