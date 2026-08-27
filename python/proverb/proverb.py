5d00cbda68ef4f03f8634a6ace1a5dba54bed8d0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-27 12:44:56 +0500

    done the solution

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -11,3 +11,15 @@ def proverb(*items, qualifier=None):
 
     if not items:
         return []
+
+    answer = []
+
+    for item in range(len(items) - 1):
+        answer.append(f"For want of a {items[item]} the {items[item + 1]} was lost.")
+
+    if not qualifier:
+        answer.append(f"And all for the want of a {items[0]}.")
+    else:
+        answer.append(f"And all for the want of a {qualifier} {items[0]}.")
+
+    return answer
