407c6ae540988cc6da9f05193ceb705378de1a03
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-28 18:11:07 +0500

    added the doc string

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,13 @@
 class Luhn:
-    def __init__(self, card_num):
-        pass
+    def __init__(self, card_num): 
+        """Initialize a card with a numeric card number. 
+        
+        Parameters: 
+            card_num (str): The card number to be converted to an integer. 
+        
+        Raises: 
+            ValueError: If card_num cannot be converted to an integer. 
+        """
 
     def valid(self):
         pass
