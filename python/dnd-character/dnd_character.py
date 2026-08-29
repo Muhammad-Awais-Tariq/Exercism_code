38570903f70c1b5bb2012cb973ee20928ffdb335
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-29 16:49:04 +0500

    done the ability function

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,7 +1,25 @@
+import random
+
 class Character:
     def __init__(self):
         pass
 
+    def ability(self):
+        """Randomly generates an ability score.
+
+        Returns:
+            int: The character's ability score.
+        """
+
+        values = []
+
+        for _ in range(4):
+            values.append(random.randint(1 , 6))
+
+        values = sorted(values)
+
+        return sum(values[1:])
+    
 def modifier(value):
     """Modifies the given value.
 
