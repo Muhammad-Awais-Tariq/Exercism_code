7a82677df579bc8ffa860af548e40a5ce6ce04f1
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-30 16:57:50 +0500

    added the docstring

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,10 @@
 class Robot:
     def __init__(self):
         pass
+
+    def name(self):
+        """Return the robot's name, generating one if necessary.
+
+        Returns:
+            str: The robot's name.
+        """
