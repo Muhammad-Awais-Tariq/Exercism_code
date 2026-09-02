066aad0440acf081353ed6e9701a16efc22a9fe5
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-02 12:55:58 +0500

    solved the exercise

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,9 +1,30 @@
 def transpose(text):
-    """Takes the transponse of the given text.
+    """Takes the transpose of the given text.
 
     Parameters:
-        text (str): The text we want transpose of.
+        text (str): The text we want to transpose.
     
     Return:
         str: The transposed text
     """
+
+    if len(text) > 0:  
+
+        words = text.split("\n")
+        max_len = max(map(len , words))
+
+        answer = [[" "] * len(words) for _ in range(max_len)]
+
+        for row in range(len(words)):
+            for column in range(max_len):
+                if column < len(words[row]):
+                    answer[column][row] = words[row][column]
+
+        rows = []
+        for row in answer:
+            row_string = "".join(row)
+            rows.append(row_string)
+
+        return "\n".join(rows)
+
+    return ""
