9c1eae47f7f87ece666ddf68de107e179bd5d738
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-02 12:39:40 +0500

    added the doc string

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,2 +1,9 @@
 def transpose(text):
-    pass
+    """Takes the transponse of the given text.
+
+    Parameters:
+        text (str): The text we want transpose of.
+    
+    Return:
+        str: The transposed text
+    """
