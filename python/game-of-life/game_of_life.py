6756c8a5b6e097fc64a6be36cd1b1f2dbfcdcc7f
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-04 10:18:49 +0500

    added the docstring

--- python/game-of-life/game_of_life.py
+++ python/game-of-life/game_of_life.py
@@ -1,2 +1,9 @@
 def tick(matrix):
-    pass
+    """Generate a new generation based on the game of life.
+
+    Parameter:
+        matrix (list) : The matrix we want to simulate life on.
+    
+    Returns:
+        list : The new generation.
+    """
