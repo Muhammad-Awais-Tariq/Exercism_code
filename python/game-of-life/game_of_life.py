1ec0681f068e8afc365e67426f44830a3edccda8
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-04 10:49:10 +0500

    done the exercise

--- python/game-of-life/game_of_life.py
+++ python/game-of-life/game_of_life.py
@@ -7,3 +7,38 @@ def tick(matrix):
     Returns:
         list : The new generation.
     """
+
+    if not matrix:
+        return []
+    
+    new_matrix = [[0]*len(matrix[0]) for _ in range(len(matrix))]
+    count = 0
+
+    for row in range(len(matrix)):
+        for column in range(len(matrix[0])):
+            if row - 1 >= 0 and  matrix[row - 1 ][column] == 1:
+                count += 1
+            if row +1 < len(matrix) and matrix[row + 1 ][column] == 1:
+                count += 1
+            if column-1 >= 0 and matrix[row][column - 1] == 1:
+                count += 1    
+            if column+1 < len(matrix[0]) and matrix[row][column + 1] == 1:
+                count += 1      
+            if row - 1 >= 0 and column -1 >= 0 and  matrix[row - 1][column -1] == 1:
+                count += 1
+            if row - 1 >= 0 and column + 1 < len(matrix[0])  and  matrix[row - 1][column + 1] == 1:
+                count += 1        
+            if row + 1 < len(matrix) and column - 1 >= 0  and  matrix[row + 1][column - 1] == 1:
+                count += 1     
+            if row + 1 < len(matrix) and column + 1 < len(matrix[0]) and  matrix[row + 1][column + 1] == 1:
+                count += 1  
+
+            if matrix[row][column] == 1 and (count == 2 or   count == 3):
+                new_matrix[row][column] = 1
+
+            if matrix[row][column] == 0 and count == 3:
+                new_matrix[row][column] = 1
+
+            count = 0
+
+    return new_matrix
