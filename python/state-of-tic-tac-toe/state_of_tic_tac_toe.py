a1125e2c0355d7a769c3272d22641adbafb157d0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-05 21:36:50 +0500

    added the doc string

--- python/state-of-tic-tac-toe/state_of_tic_tac_toe.py
+++ python/state-of-tic-tac-toe/state_of_tic_tac_toe.py
@@ -1,2 +1,9 @@
 def gamestate(board):
-    pass
+    """Tells the game state of the given game.
+
+    Parameters:
+        board(list) : The current game.
+    
+    Return:
+        str: The game state.
+    """    
