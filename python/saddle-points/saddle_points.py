352b703cbe93326fc7041f0b10ee2891d5e800b5
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-06 19:53:10 +0500

    solved the exercise

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -7,3 +7,26 @@ def saddle_points(matrix):
     Return:
         list: The found saddle points
     """
+
+    answer = []
+    if not matrix:
+        return answer
+
+    first_row_len = len(matrix[0])
+    for idx in range(1,len(matrix)):
+        if len(matrix[idx]) != first_row_len:
+            raise ValueError("irregular matrix")
+        
+    for i , rows in enumerate(matrix):
+        row_max = max(rows)
+
+        max_idx = [index for index , value in enumerate(rows) if value == row_max]
+
+        for max_col in max_idx:
+            for row in range(len(matrix)):
+                if matrix[row][max_col] < row_max:
+                    break
+            else:     
+                answer.append({"row": i+1, "column": max_col+1})
+
+    return answer
