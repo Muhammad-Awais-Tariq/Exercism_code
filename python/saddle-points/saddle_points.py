edd4627f49822ed7416597e0d12666aed5b6c570
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-06 19:24:28 +0500

    added the doc strin

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,9 @@
 def saddle_points(matrix):
-    pass
+    """give the saddle points based on the matrix.
+
+    Paramters:
+        matrix (list): The matrix to find the positions.
+    
+    Return:
+        list: The found saddle points
+    """
