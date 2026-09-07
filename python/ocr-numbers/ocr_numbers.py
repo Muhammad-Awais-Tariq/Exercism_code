8a686c3e23c76161b4ca0df9ecb4a7d704170d13
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-07 17:18:53 +0500

    added the exceptions

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -8,3 +8,8 @@ def convert(input_grid):
         int: The recognized digit.
     """
 
+    if len(input_grid) % 4 != 0:
+        raise ValueError("Number of input lines is not a multiple of four")
+
+    if len(input_grid[0]) % 3 != 0:
+        raise ValueError("Number of input columns is not a multiple of three")
