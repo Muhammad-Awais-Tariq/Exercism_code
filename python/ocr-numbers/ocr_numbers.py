889265a572933f6acdcea24dad6dcb6c22350b34
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-07 18:33:33 +0500

    solved the exercise

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -13,3 +13,54 @@ def convert(input_grid):
 
     if len(input_grid[0]) % 3 != 0:
         raise ValueError("Number of input columns is not a multiple of three")
+
+    pattern_dict = {
+        (" _ ", "| |", "|_|", "   "): "0",
+        ("   ", "  |", "  |", "   "): "1",
+        (" _ ", " _|", "|_ ", "   "): "2",
+        (" _ ", " _|", " _|", "   "): "3",
+        ("   ", "|_|", "  |", "   "): "4",
+        (" _ ", "|_ ", " _|", "   "): "5",
+        (" _ ", "|_ ", "|_|", "   "): "6",
+        (" _ ", "  |", "  |", "   "): "7",
+        (" _ ", "|_|", "|_|", "   "): "8",
+        (" _ ", "|_|", " _|", "   "): "9",
+    }
+
+    if len(input_grid[0]) == 3:
+        try:
+            return pattern_dict[tuple(input_grid)]
+        except KeyError:
+            return "?"
+
+    if len(input_grid) > 4:
+        full_final_word = ""
+        for i in range(0 , len(input_grid) , 4):
+            current_word = input_grid[i:i+4]
+            for j in range(0 , len(current_word[0]) , 3):
+                temp_word = []
+                for k in range(len(current_word)):
+                    temp_word.append(current_word[k][j:j+3])            
+                try:
+                    full_final_word += f"{pattern_dict[tuple(temp_word)]}"
+                except KeyError:
+                    full_final_word += "?"
+
+            if i != len(input_grid) - 4:
+                full_final_word += ","
+
+        return full_final_word
+    
+    else:
+        final_word = ""
+        for j in range(0 , len(input_grid[0]) , 3):
+            current_word = []
+            for i in range(len(input_grid)):
+                current_word.append(input_grid[i][j:j+3])
+
+            try:
+                final_word += pattern_dict[tuple(current_word)]
+            except KeyError:
+                final_word += "?"
+
+        return final_word
