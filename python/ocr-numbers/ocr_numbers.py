156e8f2a70924402654098cb3dadabd95aa7b345
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-07 18:34:23 +0500

    fixed the function

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -1,11 +1,15 @@
 def convert(input_grid):
-    """Converts the input grid into the numbers.
+    """Convert an OCR grid into a string of recognized digits.
 
     Parameters:
-        input_grid (list) : The given chracter.
-    
+        input_grid (list): The OCR grid containing digit patterns.
+
     Returns:
-        int: The recognized digit.
+        str: The recognized digits, with commas separating groups.
+
+    Raises:
+        ValueError: If the number of input lines is not a multiple of four
+            or the number of input columns is not a multiple of three.
     """
 
     if len(input_grid) % 4 != 0:
@@ -35,32 +39,41 @@ def convert(input_grid):
 
     if len(input_grid) > 4:
         full_final_word = ""
-        for i in range(0 , len(input_grid) , 4):
-            current_word = input_grid[i:i+4]
-            for j in range(0 , len(current_word[0]) , 3):
-                temp_word = []
-                for k in range(len(current_word)):
-                    temp_word.append(current_word[k][j:j+3])            
+
+        for group_start in range(0, len(input_grid), 4):
+            current_group = input_grid[group_start:group_start + 4]
+
+            for column_start in range(0, len(current_group[0]), 3):
+                digit_pattern = []
+
+                for row_index in range(len(current_group)):
+                    digit_pattern.append(
+                        current_group[row_index][column_start:column_start + 3]
+                    )
+
                 try:
-                    full_final_word += f"{pattern_dict[tuple(temp_word)]}"
+                    full_final_word += pattern_dict[tuple(digit_pattern)]
                 except KeyError:
                     full_final_word += "?"
 
-            if i != len(input_grid) - 4:
+            if group_start != len(input_grid) - 4:
                 full_final_word += ","
 
         return full_final_word
-    
-    else:
-        final_word = ""
-        for j in range(0 , len(input_grid[0]) , 3):
-            current_word = []
-            for i in range(len(input_grid)):
-                current_word.append(input_grid[i][j:j+3])
 
-            try:
-                final_word += pattern_dict[tuple(current_word)]
-            except KeyError:
-                final_word += "?"
+    final_word = ""
 
-        return final_word
+    for column_start in range(0, len(input_grid[0]), 3):
+        digit_pattern = []
+
+        for row_index in range(len(input_grid)):
+            digit_pattern.append(
+                input_grid[row_index][column_start:column_start + 3]
+            )
+
+        try:
+            final_word += pattern_dict[tuple(digit_pattern)]
+        except KeyError:
+            final_word += "?"
+
+    return final_word
