a929bedc0ca2275949081398b4b9204f83be4158
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-07 17:11:40 +0500

    added the docstring

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -1,3 +1,10 @@
 def convert(input_grid):
-    pass
+    """Converts the input grid into the numbers.
+
+    Parameters:
+        input_grid (list) : The given chracter.
+    
+    Returns:
+        int: The recognized digit.
+    """
 
