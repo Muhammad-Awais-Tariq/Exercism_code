430017cadf998e657f2a5032bb73c3d34548a105
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-08 21:29:58 +0500

    done the final version

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -5,28 +5,25 @@ SOUTH = 2
 
 
 class Robot:
-    """
-    A robot class to handle the robot movements.
-    """
+    """Represent a robot that can move around a grid."""
+
     def __init__(self, direction=NORTH, x_pos=0, y_pos=0):
-        """Construtor for intilization.
+        """Initialize the robot with a direction and coordinates.
 
         Parameters:
-            direction: The start direction.
-            x_pos : The x position.
-            y_post: The y position
+            direction (int): The robot's starting direction.
+            x_pos (int): The robot's starting x-coordinate.
+            y_pos (int): The robot's starting y-coordinate.
         """
-
         self.direction = direction
-        self.coordinates = (x_pos , y_pos)
+        self.coordinates = (x_pos, y_pos)
 
-    def move(self , moves):
-        """Moves the robot based on the given moves.
+    def move(self, moves):
+        """Move the robot according to the given instructions.
 
         Parameters:
-            moves(str) : The string containing all the moves.
+            moves (str): A string containing the movement instructions.
         """
-        
         for move in moves:
             if move == "R":
                 if self.direction != 3:
@@ -38,16 +35,16 @@ class Robot:
                 if self.direction != 0:
                     self.direction -= 1
                 else:
-                    self.direction = 3    
+                    self.direction = 3
 
             if move == "A":
-                x , y = self.coordinates
-                if self.direction == 0:
-                    self.coordinates = (x,y+1)        
-                elif self.direction == 1:
-                    self.coordinates = (x+1,y)    
-                elif self.direction == 2:
-                    self.coordinates = (x,y-1)   
-                else:
-                    self.coordinates = (x-1,y)    
+                x, y = self.coordinates
 
+                if self.direction == 0:
+                    self.coordinates = (x, y + 1)
+                elif self.direction == 1:
+                    self.coordinates = (x + 1, y)
+                elif self.direction == 2:
+                    self.coordinates = (x, y - 1)
+                else:
+                    self.coordinates = (x - 1, y)
