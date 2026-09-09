4646b20a4e26c20574de446b81c467588a9d7c2e
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-09 19:45:43 +0500

    done the first function

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,10 +1,23 @@
 class School:
     """ The school class. """
     def __init__(self):
-        pass
+        """
+        Constructor
+        """
+        self.students = {}
 
     def add_student(self, name, grade):
-        pass
+        """Adds the students to the school dict
+        
+        """
+
+        all_names = [name for names in self.students.values() for name in names]
+        if name not in all_names:
+            if grade in self.students:
+                self.students[grade].append(name)
+            else:
+                self.students[grade] = [name]
+        
 
     def roster(self):
         pass
