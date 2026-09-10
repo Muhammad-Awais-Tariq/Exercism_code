ec17ce024353f533c23e7b099a93f52e93176cfd
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-10 19:55:05 +0500

    done the one solution

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -7,3 +7,18 @@ def primes(limit):
     Returns:
         list : All the prime numbers.
     """
+
+    if limit < 2:
+        return []
+    
+    primes = [2]
+
+    for num in range(3,limit+1):
+        is_prime = True
+        for prime in primes:
+            if num % prime == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            primes.append(num)
