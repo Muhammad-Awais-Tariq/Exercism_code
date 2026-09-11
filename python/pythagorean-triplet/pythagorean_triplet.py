83e9c645ac96c1ceb3415b41c1859628430b3e5a
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-11 18:34:36 +0500

    added the doc string

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,2 +1,9 @@
 def triplets_with_sum(number):
-    pass
+    """Return the pythagorean triplet whose sum is equal to the given number.
+
+    Paramters:
+        number (int): The number which sum we want.
+    
+    Returns:
+        (list) : The numbers whose sum is equal to the given number 
+    """
