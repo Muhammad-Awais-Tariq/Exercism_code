a14f16b7e330b79effc4f8ba01a0d9497134f907
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-11 18:51:58 +0500

    done the solution

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -7,3 +7,14 @@ def triplets_with_sum(number):
     Returns:
         (list) : The numbers whose sum is equal to the given number 
     """
+
+    numbers = []
+
+    for a in range(1, number-3):
+        for b in range(a , number):
+            c = number - a - b
+            if c > b:
+                if (a ** 2) + (b ** 2) == (c** 2):
+                    numbers.append([a,b,c])
+
+    return numbers
