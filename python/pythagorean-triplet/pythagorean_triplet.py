a07de8506590fc5c9dbe84f1b45b79941bd59eb5
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-11 19:26:06 +0500

    added the optimized code

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -8,14 +8,10 @@ def triplets_with_sum(number):
         list: A list of Pythagorean triplets whose elements sum to `number`.
     """
 
-    triplets = []
-
-    for first_side in range(1, number - 3):
-        for second_side in range(first_side, number):
-            third_side = number - first_side - second_side
-
-            if third_side > second_side:
-                if (first_side ** 2) + (second_side ** 2) == third_side ** 2:
-                    triplets.append([first_side, second_side, third_side])
-
-    return triplets
+    result = []
+    for a in range(1, number // 3 + 1):
+        for b in range(a, (number - a) // 2 + 1):
+            c = number - a - b
+            if a * a + b * b == c * c:
+                result.append([a, b, c])
+    return result
