64868b9251aebbc97507e0c0282afc3c6f577aa6
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-13 15:46:27 +0500

    done the constructor

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,7 +1,12 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
-
+        self.final_row = []
+        for row in matrix_string.split("\n"):
+            current_row = []
+            for element in row.split(" "):
+                    current_row.append(int(element))
+            self.final_row.append(current_row)
+        
     def row(self, index):
         pass
 
