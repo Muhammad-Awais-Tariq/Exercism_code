47d4c80175fcea80541dea7a26c0fca5d8d0482c
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-15 20:52:50 +0500

    added the doc string

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,8 @@
 class Garden:
     def __init__(self, diagram, students):
-        pass
+        """Constructor to intilize the digram and students.
+
+        Parameters:
+            digram (str): The plants digram.
+            students (list): The students names.
+        """
