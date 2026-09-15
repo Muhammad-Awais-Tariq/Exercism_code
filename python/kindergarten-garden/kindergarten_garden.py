8490c9dc8a3f48df1da42c6ee219350736df54d0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-15 20:57:26 +0500

    done the constructor

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,8 +1,12 @@
 class Garden:
-    def __init__(self, diagram, students):
+    def __init__(self, diagram, students=["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]):
         """Constructor to intilize the digram and students.
 
         Parameters:
             digram (str): The plants digram.
             students (list): The students names.
         """
+
+        self.diagram = diagram
+        self.students = students
+        self.digram_rows = diagram.split("\n")
