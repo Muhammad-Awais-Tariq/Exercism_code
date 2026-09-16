d9743bbb9265f1917a6c9e26029f88dd856c6c91
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-16 19:32:49 +0500

    added the doc string

--- python/bottle-song/bottle_song.py
+++ python/bottle-song/bottle_song.py
@@ -1,2 +1,7 @@
 def recite(start, take=1):
-    pass
+    """Recite the verses based on the start index.
+
+    Parameters:
+        start (int): The start index to start the verse from.
+        take (int): How many verses we want.
+    """
