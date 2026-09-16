a8277f05464361e1a7a81a07e549627726a0f3b1
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-16 19:51:11 +0500

    solved the question

--- python/bottle-song/bottle_song.py
+++ python/bottle-song/bottle_song.py
@@ -7,6 +7,7 @@ def recite(start, take=1):
     """
 
     number_words = {
+        0: "no",
         1: "One",
         2: "Two",
         3: "Three",
@@ -19,4 +20,25 @@ def recite(start, take=1):
         10: "Ten",
     }
 
-    
+    verses = []
+    current = 0
+    for i in range(start , 0 , -1):
+        bottle = "bottle" if i == 1 else "bottles"
+        next_bottle = "bottle" if i - 1 == 1 else "bottles"
+        verses.extend([
+            f"{number_words[i]} green {bottle} hanging on the wall,",
+            f"{number_words[i]} green {bottle} hanging on the wall,",
+            "And if one green bottle should accidentally fall,",
+            f"There'll be {number_words[i - 1].lower()} green {next_bottle} hanging on the wall.",
+        ])
+        
+
+        current += 1
+
+        if current == take:
+            break
+
+        if i != 1:
+            verses.append("")        
+
+    return verses
