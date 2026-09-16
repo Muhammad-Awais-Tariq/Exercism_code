13242a3967c461fc9ae41d663d27b176911c36c6
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-16 19:33:50 +0500

    Added the word mapping

--- python/bottle-song/bottle_song.py
+++ python/bottle-song/bottle_song.py
@@ -5,3 +5,18 @@ def recite(start, take=1):
         start (int): The start index to start the verse from.
         take (int): How many verses we want.
     """
+
+    number_words = {
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+        10: "Ten",
+    }
+
+    
