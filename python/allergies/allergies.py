067e3c7f0e9d4c6c6395812393c2cd06d0a666ba
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-17 20:49:44 +0500

    added the mapping

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -8,7 +8,17 @@ class Allergies:
         """
 
         self.score = score
-
+        self.mapping = {
+            "eggs": 1,
+            "peanuts": 2,
+            "shellfish": 4,
+            "strawberries": 8,
+            "tomatoes": 16,
+            "chocolate": 32,
+            "pollen": 64,
+            "cats": 128,
+        }
+        
     def allergic_to(self, item):
         pass
 
