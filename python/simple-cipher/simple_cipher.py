4b1970128b5d84a39a8bdb99314fe701ea40d515
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-18 20:08:09 +0500

    added the doc string

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 class Cipher:
     def __init__(self, key=None):
         """Constructor for the object intialization.
@@ -6,8 +9,17 @@ class Cipher:
             key(str): The based on which we will generate the text.
         """
 
+        if key is None:
+            self.key = "".join(random.choices(string.ascii_lowercase , k=100))
+        else:
+            self.key = key
+
     def encode(self, text):
-        pass
+        """Encode the text based on the key provided by the user.
+
+        Parameters:
+            text (str): The text that we want to encode
+        """
 
     def decode(self, text):
         pass
