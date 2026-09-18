a55931bc622dd6e231bc399e59b70de4b72b2672
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-18 21:01:48 +0500

    done the encode

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -21,5 +21,15 @@ class Cipher:
             text (str): The text that we want to encode
         """
 
+        shift = ord(self.key) - ord("a")
+
+        new_txt = ""
+
+        for char in text:
+            new_char = (ord(char) - ord("a") + shift) % 26
+            new_txt += chr(new_char + ord("a"))
+
+        return new_txt
+
     def decode(self, text):
         pass
