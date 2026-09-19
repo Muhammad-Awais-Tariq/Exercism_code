4daaa2f82778e623e3f2690efbac961b11ca4b35
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-19 19:37:05 +0500

    done the basic code thats required

--- python/poker/poker.py
+++ python/poker/poker.py
@@ -7,3 +7,48 @@ def best_hands(hands):
     returns:
         list: The best hand
     """
+
+    for hand in hands:
+        current_hand = hand.split(" ")
+        current_rank = []
+        suits = []
+        for current in current_hand:
+            current_rank.append(current[:-1])
+            suits.append(current[-1])
+
+        hash_map = {
+            "J" : 11,
+            "Q" : 12,
+            "K" : 13,
+            "A" : 14
+        }
+
+        for i in range(len(current_rank)):
+            if current_rank[i].isnumeric():
+                current_rank[i] = int(current_rank[i])
+            else:
+                current_rank[i] = hash_map[current_rank[i]]
+
+        current_rank = sorted(current_rank , reverse=True)
+
+        rank_counter = {}
+
+        for num in current_rank:
+            if num in rank_counter:
+                rank_counter[num] +=  1
+            else:
+                rank_counter[num] = 1
+
+        if set(suits) == 1:
+            pass
+
+        straight = True
+        
+        for i in range(len(current_rank)-1):
+            if current_rank[i] != current_rank[i+1] + 1:
+                straight = False
+                break
+
+        if current_rank == [14, 5, 4, 3, 2]:
+            straight = True
+            
