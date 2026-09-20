af12f2c671e073a654c74af6ecba4d5d5ae55aea
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-20 11:31:52 +0500

    fixed the docstring

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,38 +1,45 @@
 def answer(question):
-    """Takes the question and returns the answer.
+    """Parse and evaluate a word-based math question.
 
     Parameters:
-        question (str): The question that we want solution to.
+        question (str): A word-based math question.
 
     Returns:
-        int: The answer.
+        int: The result of the calculation.
+
+    Raises:
+        ValueError: If the question has invalid syntax or an
+            unknown operation.
     """
 
-    question_split = question.replace(" by" , "").split(" ")
-    question_split[-1] = question_split[-1].replace("?" , "")
+    question_split = question.replace(" by", "").split(" ")
+    question_split[-1] = question_split[-1].replace("?", "")
+
     try:
         operands = list(map(int, question_split[2::2]))
     except ValueError:
         raise ValueError("syntax error")
-    
+
     operators = question_split[3::2]
+
     if len(operands) < 1:
         raise ValueError("syntax error")
-    
+
     total = operands[0]
 
     for operator in operators:
         if operator.isnumeric():
             raise ValueError("syntax error")
-        if operator not in ["plus" , "minus" , "multiplied" , "divided"]:
+
+        if operator not in ["plus", "minus", "multiplied", "divided"]:
             raise ValueError("unknown operation")
 
     if len(operands) != len(operators) + 1:
         raise ValueError("syntax error")
 
-    for i in range(len(operands)-1):
+    for i in range(len(operands) - 1):
         operator = operators[i]
-        operand_2 = operands[i+1]
+        operand_2 = operands[i + 1]
 
         if operator == "plus":
             total += operand_2
