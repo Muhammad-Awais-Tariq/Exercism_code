6cd2f6cafe18d85df324b717d63fbb05f812b11d
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-20 11:05:51 +0500

    done the basic version

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -7,3 +7,27 @@ def answer(question):
     Returns:
         int: The answer.
     """
+
+    question_split = question.replace(" by" , "").split(" ")
+    question_split[-1] = question_split[-1].replace("?" , "")
+    operands = list(map(int, question_split[2::2]))
+    operators = question_split[3::2]
+    total = operands[0]
+
+    for i in range(len(operands)-1):
+        operator = operators[i]
+        operand_2 = operands[i+1]
+
+        if operator == "plus":
+            total += operand_2
+        elif operator == "minus":
+            total -= operand_2
+        elif operator == "multiplied":
+            total *= operand_2
+        elif operator == "divided":
+            division = total / operand_2
+            total = int(division)
+
+    return total
+
+print(answer("What is 5 plus 13 plus 6?"))
