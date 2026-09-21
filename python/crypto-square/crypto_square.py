0e0c96f16b8777020b16e2ae96f205391348d87d
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-21 19:07:58 +0500

    Added the doc string

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,2 +1,9 @@
 def cipher_text(plain_text):
-    pass
+    """Cipher the text from the plain to the ciphered.
+
+    Parameters:
+        plain_text (str): The plain text to be ciphered.
+    
+    Returns:
+        str: The chipered text.
+    """
