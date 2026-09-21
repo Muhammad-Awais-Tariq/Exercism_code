cabb3d6a30edef9b85a58bfee2f34ad62b5b2596
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-21 19:44:01 +0500

    fixed the code

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -11,9 +11,26 @@ def cipher_text(plain_text):
         str: The chipered text.
     """
 
+    
     clean_text = plain_text.translate(str.maketrans('', '', string.punctuation))
     clean_text = clean_text.replace(" " , "").lower()
 
+    if not clean_text:
+        return ""
+    
     columns = math.ceil(math.sqrt(len(clean_text)))
 
     rows = math.ceil(len(clean_text) / columns)
+
+    result = ""
+
+    for i in range(columns):
+        for j in range(rows):
+            index = j * columns + i
+            try:
+                result += clean_text[index]
+            except IndexError:
+                result += " "
+        result += " "
+
+    return result
