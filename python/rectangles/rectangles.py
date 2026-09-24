a7c46eb71dd1c91af1c20abafae6999404bfd1d0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-24 12:50:14 +0500

    added the doc string

--- python/rectangles/rectangles.py
+++ python/rectangles/rectangles.py
@@ -1,2 +1,9 @@
 def rectangles(strings):
-    pass
+    """Tell the number of rectangles based on the ascii characters.
+
+    Parameters:
+        strings(str): The ascii chracters based on which we will check.
+    
+    Returns:
+        int: The number of the rectangles.
+    """
